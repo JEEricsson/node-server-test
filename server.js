0a78b5ff74abf7a146d6a1098a8ed536a13fcaa7
JE Ericsson <Crypt055@Macintosh.local>
2018-04-15 10:07:15 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const hbs =require('hbs')
+const fs = require('fs')
+
+let app = express()
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs')
+
+
+app.use((req, res, next) => {
+    let now = new Date().toString()
+    let log= `${now}: ${req.method}: ${req.url}`
+    console.log(log)
+    fs.appendFile('server.log',log +'\n', (err) => {
+        if (err){
+            console.log('Unable to append to server.log.')
+        }
+    })
+    next()
+})
+
+// app.use((req,res,next)=> {
+//     res.render('maintenance.hbs', {
+//         currentTitle: 'Maintenance',
+//         welcomeMessage: 'We will be right back',
+       
+//     })
+// })
+
+app.use(express.static(__dirname + '/public'))
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase()
+})
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        currentTitle: 'Home Page',
+        welcomeMessage: 'Welcome to the home page',
+       
+    })
+    
+})
+
+app.get('/about', (req, res) =>{
+    res.render('about.hbs', {
+        currentTitle: 'About Page',
+       
+    })
+})
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: {
+            type: 'error',
+            message: 'OOPS Something went wrong'
+        } 
+        
+    })
+})
+app.listen(3000, () => {
+    console.log('Server is upp and running')
+})
